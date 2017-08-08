15adb07df71bc997f58d0b2354221799bce4b4b4
Monte Ohrt <monteo@zillow.com>
2017-08-08 11:22:10 -0500

    add image/url

--- bitcoincash.1m.sh
+++ bitcoincash.1m.sh
@@ -6,8 +6,8 @@
 # <bitbar.author>Monte Ohrt</bitbar.author>
 # <bitbar.author.github>mohrt</bitbar.author.github>
 # <bitbar.desc>Simply shows the value of BitcoinCash in USD</bitbar.desc>
-# <bitbar.image></bitbar.image>
-# <bitbar.abouturl></bitbar.abouturl>
+# <bitbar.image>https://raw.githubusercontent.com/mohrt/bitbar-bitcoincash/master/bitcoin-cash.png</bitbar.image>
+# <bitbar.abouturl>https://github.com/mohrt/bitbar-bitcoincash</bitbar.abouturl>
 #
 # Text above --- will be cycled through in the menu bar,
 # whereas text underneath will be visible only when you
